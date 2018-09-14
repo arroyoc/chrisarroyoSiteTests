a0d33049139c7a9820c7cd20a8d7f0a90c1c7c7d
Chris Arroyo <arroyoc91@gmail.com>
2018-09-14 16:57:48 -0600

    created Account Login page object class

---
+++ src/test/java/pageObjects/ChrisArroyoApp/AccountLoginScreen.java
@@ -0,0 +1,11 @@
+package pageObjects.ChrisArroyoApp;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidElement;
+
+public class AccountLoginScreen extends MobilePageObjectsBase {
+
+    public AccountLoginScreen(AppiumDriver<AndroidElement> driver){
+        super(driver);
+    }
+}
