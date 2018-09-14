4a87d84cfc1b061009dde219d72ffa6b79512509
Chris Arroyo <arroyoc91@gmail.com>
2018-09-14 16:38:14 -0600

    removed properties in testbase

--- src/test/java/testBase/TestBase.java
+++ src/test/java/testBase/TestBase.java
@@ -12,24 +12,9 @@ public class TestBase {
 
     protected static WebDriver wdriver;
 
-    private String chrisarroyoUrl;
-
-
-    private void loadConfig() throws IOException{
-
-        //Config property file
-        Properties CONFIG = new Properties();
-        FileInputStream fn = new FileInputStream(System.getProperty("user.dir")+"//src//config/config.properties");
-        CONFIG.load(fn);
-
-        chrisarroyoUrl = CONFIG.getProperty("chrisarroyoUrl");
-
-    }
 
     // method to start webdriver
     protected void webSetup() throws IOException{
-
-        loadConfig();
 
         //Check if web driver has already been instantiated
         //Driver options are 'PhantomJSDriver', 'ChromeDriver', 'FireFoxDriver'
@@ -47,6 +32,7 @@ public class TestBase {
 
     //Method that loads chrisarroyo.info
     private void loadResumeSite(){
+        String chrisarroyoUrl = "http://chrisarroyo.info";
         wdriver.get(chrisarroyoUrl);
         wdriver.manage().window().maximize();
     }
