38ca7b95a9db47c8e6059aa65f41427075591a8e
Chris Arroyo <arroyoc91@gmail.com>
2018-09-14 16:46:42 -0600

    added mobile setup

--- src/test/java/testBase/TestBase.java
+++ src/test/java/testBase/TestBase.java
@@ -1,5 +1,7 @@
 package testBase;
 
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
 import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -7,10 +9,12 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Properties;
 
 public class TestBase {
 
+    protected static AndroidDriver<AndroidElement> mdriver;
     protected static WebDriver wdriver;
 
     private static DesiredCapabilities getDesiredCapabilities() {
@@ -31,6 +35,26 @@ public class TestBase {
         capabilities.setCapability("newCommandTimeout", commandTimeout);
 
         return capabilities;
+    }
+
+    public static void mobileSetup() throws IOException{
+
+        String appiumDriverURL="http://127.0.0.1:4723/wd/hub";
+
+        //Setup Appium
+        DesiredCapabilities capabilities = getDesiredCapabilities();
+
+        //Check if mobile driver has already been instantiated
+        if(mdriver == null){
+            mdriver = new AndroidDriver<>(new URL(appiumDriverURL), capabilities);
+
+            try {
+                //Give the app time to load
+                Thread.sleep(20000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     // method to start webdriver
